2d27d04cb1701c424f1d20dabde2e4b4dbd91bff
Jayanth Ganapathi <jayanthganapathi04@gmail.com>
2024-07-12 14:57:33 +0530

    Create word.java

---
+++ word.java
@@ -0,0 +1,9 @@
+public class PrintWord {
+    public static void main(String[] args) {
+        // Define the word to be printed
+        String word = "Hello, World!";
+
+        // Print the word to the console
+        System.out.println(word);
+    }
+}
